a5c246332ed32a426193bc6eb74432b3e3fe9056
SASUKE40 <SASUKE688848@gmail.com>
2020-06-07 04:23:26 +0000

    Deploying to master from  @ f68d6dd4bd0456da9fac731cf3e7fe684ce697ce 🚀

---
+++ component---src-pages-404-js-149a956ec8c4b0d047a0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{w2l6:function(n,t,e){"use strict";e.r(t);e("q1tI");var d=e("dH/O"),l=e("noox"),o=e("WsIH"),u=e("gZs9"),a=e("qKvR");t.default=function(){return Object(a.d)(d.a,null,Object(a.d)(o.a,null),Object(a.d)(l.a,null,Object(a.d)("div",{style:{marginTop:"100px"}},Object(a.d)(u.a.h1,null,"404: Page Not Found"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-149a956ec8c4b0d047a0.js.map
